13c2c9b0b6684df4d46a7090d3648c8c6a5a6426
vwinnik <winnikvanina@gmail.com>
2021-04-09 00:39:27 -0300

    Add files.

--- src/main_app/index.js
+++ src/main_app/index.js
@@ -2,13 +2,12 @@ import { loader } from 'graphql.macro'
 import { useQuery } from '@apollo/client'
 
 import client from 'services/graphql/client'
-import SignUp from './components/SignUp' 
+import SignUp from './components/SignUp'
 
 const exampleQuery = loader('./graphql/currentUser.graphql')
 
 function App () {
   const { loading, error, data } = useQuery(exampleQuery, { client })
-
   console.log(loading, error, data)
 
   return (
